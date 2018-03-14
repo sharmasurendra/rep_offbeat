287d80ac47e1b3dbe58a3be0b413ece3e5316498
sharmasurendra <37316556+sharmasurendra@users.noreply.github.com>
2018-03-14 15:45:08 -0700

    Update menu.php

--- src/common/menu.php
+++ src/common/menu.php
@@ -1,8 +1,13 @@
 		           <div class="col-md-3">
-                <p class="lead">Start Here</p>
+                <p class="lead">Collections</p>
                 <div class="list-group">
-                    <a href="#" class="list-group-item">Buy Anything</a>
-                    <a href="#" class="list-group-item">Sell Anything</a>
-                    <a href="#" class="list-group-item">Trade Products</a>
+                    <a href="#" class="list-group-item">Bar Necklace</a>
+                    <a href="#" class="list-group-item">Bridal</a>
+                    <a href="#" class="list-group-item">Charms</a>
+			<a href="#" class="list-group-item">Gems</a>
+			<a href="#" class="list-group-item">Pearl</a>
+			<a href="#" class="list-group-item">Pendant</a>
+			<a href="#" class="list-group-item">Hoops</a>
+			<a href="#" class="list-group-item">Studs</a>
                 </div>
             </div>
