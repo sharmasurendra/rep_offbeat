1cd49bed7f18ab855d51afd80fac3d111d2ed71f
vkodwani1 <vkodwani@salesforce.com>
2018-04-27 16:50:41 -0700

    update desc

--- src/product-description.php
+++ src/product-description.php
@@ -13,19 +13,16 @@
 
  try {
     echo "111";
-    // $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
-    // // set the PDO error mode to exception
-    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
 
-    //  $stmt = $conn->prepare("SELECT * FROM products where 1
-    // and product_id= :value" );
-
-    // $stmt->bindParam(':value', $_POST['action']);
-    // $stmt->execute();
-    // $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    // $result = $stmt->fetch();
-    // $value = $result['product_name'];
-    // echo "222";
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM products where 1 and product_id= :value" );
+    $stmt->bindParam(':value', $_POST['action']);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetch();
+    $value = $result['product_name'];
+    echo "222";
 
 
 }
