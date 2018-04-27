dd2b6571ab4cd2c19ae699d71b406068163c254d
vkodwani1 <vkodwani@salesforce.com>
2018-04-27 12:27:47 -0700

    update all users for curl

--- src/allUsers.php
+++ src/allUsers.php
@@ -6,6 +6,14 @@ $myDataBaseName = "heroku_5eaa584d7cda171";
 
 
 
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$username = "b1069ce4ee0339";
+$password = "7ee6e563";
+//$username = "root";
+//$password = "";
+$dbname = "heroku_5eaa584d7cda171";
+
+
 
 $con = mysqli_connect( "$myHost", "$myUserName", "$myPassword", "$myDataBaseName" );
 $cumurow = array();
@@ -15,7 +23,7 @@ if( !$con ) // == null if creation of connection object failed
     // report the error to the user, then exit program
     // die("connection object not created: ".mysqli_error($con));
 } else{
-      $result = mysqli_query($con, "Select firstName,lastName from `Users`");
+      $result = mysqli_query($con, "Select first_name,last_name from `User`");
        while ($row = mysqli_fetch_row($result)) {
         array_push($cumurow, $row);
         // $users = $users.",".$row[0];
