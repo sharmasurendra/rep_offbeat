f215a4452fd71e6f3510100ce8509b07fa1d5bc2
vkodwani1 <vkodwani@salesforce.com>
2018-04-27 17:22:31 -0700

    4

--- src/product-description.php
+++ src/product-description.php
@@ -19,23 +19,7 @@
     $result = $stmt->fetch();
     $value = $result['product_name'];
 
-    if(isset($_COOKIE['cookie']))
-    {
-        echo "111";
-        $last = $result['product_name']  .",". $_COOKIE["last_visited"];
-        echo "$last";
-        
-    if(isset(($_COOKIE['cookie'])["$value"]))
-           $count = ($_COOKIE['cookie'])["$value"] + 1;
-        else
-          $count = 1;
-    }
-    else
-    {
-        $last = $result['product_name'];
-        $count = 1;
-    }
-
+   
     }
     catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
